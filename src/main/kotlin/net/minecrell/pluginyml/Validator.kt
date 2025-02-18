811746e2706037f490dbde646aff6bd4c776dce4
Lilly <46890129+RainbowDashLabs@users.noreply.github.com>
2025-02-18 17:25:51 +0100

    Add spdx header

--- src/main/kotlin/net/minecrell/pluginyml/Validator.kt
+++ src/main/kotlin/net/minecrell/pluginyml/Validator.kt
@@ -1,3 +1,10 @@
+/*
+ *     SPDX-License-Identifier: MIT
+ *
+ *     Copyright (c) 2017 Minecrell <https://github.com/Minecrell>
+ *     Copyright (c) 2024 EldoriaRPG and Contributor <https://github.com/eldoriarpg>
+ */
+
 package net.minecrell.pluginyml
 
 
